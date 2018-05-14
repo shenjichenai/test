611b28848a1c961c0cde7d38ce427f33a7c3dfa8
0 <0@0-PC>
2018-05-14 09:11:47 +0800

    测试类

--- Test/src/test/java/com/yida/Test.java
+++ Test/src/test/java/com/yida/Test.java
@@ -1,14 +1,5 @@
 package com.yida;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-
-import com.yida.utils.StreamUtils;
-
 /**
  * Copyright  2010-2015 四川数码物联网络科技有限责任公司. All Rights Reserved.
  */
@@ -22,28 +13,27 @@ import com.yida.utils.StreamUtils;
  */
 public class Test {
 	public static void main(String[] args) {
-		File file = new File("C:\\Users\\0\\Desktop\\打包.zip");
-		InputStream in = null;
-		ByteArrayOutputStream bos = null;
-		try {
-			in = new FileInputStream("C:\\Users\\0\\Desktop\\打包.zip");
-			StreamUtils.inputstreamToFile(in, new File("C:\\Users\\0\\Desktop\\temp\\" + file.getName()));
-			System.err.println("success");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} finally {
-			if (bos != null) {
-				try {
-					bos.close();
-				} catch (IOException e) {
-				}
-			}
-			if (in != null) {
-				try {
-					in.close();
-				} catch (IOException e) {
-				}
+		int[] arr = new int[] { 12, 23, 34, 45, 56, 67, 77, 89, 90 };
+		System.out.println(search(arr, 12));
+		System.out.println(search(arr, 45));
+		System.out.println(search(arr, 67));
+		System.out.println(search(arr, 89));
+		System.out.println(search(arr, 99));
+	}
+
+	public static int search(int[] arr, int key) {
+		int start = 0;
+		int end = arr.length - 1;
+		while (start <= end) {
+			int middle = (start + end) / 2;
+			if (key < arr[middle]) {
+				end = middle - 1;
+			} else if (key > arr[middle]) {
+				start = middle + 1;
+			} else {
+				return middle;
 			}
 		}
+		return -1;
 	}
 }
