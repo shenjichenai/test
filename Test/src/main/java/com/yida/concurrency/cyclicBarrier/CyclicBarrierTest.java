c2b6dcc443b5c865a78911b6f238c93b4f74af7f
0 <0@0-PC>
2018-06-04 18:07:56 +0800

    新增CyclicBarrier使用

---
+++ Test/src/main/java/com/yida/concurrency/cyclicBarrier/CyclicBarrierTest.java
@@ -0,0 +1,66 @@
+package com.yida.concurrency.cyclicBarrier;
+
+import java.util.Map;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CyclicBarrier;
+
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
+/**
+ *********************
+ * @author yangke
+ * @version 1.0
+ * @created 2018年6月4日 下午4:14:13
+ ***********************
+ */
+public class CyclicBarrierTest {
+
+	private final Map<String, Object> map = new ConcurrentHashMap<String, Object>();
+
+	class Player implements Runnable {
+
+		private CyclicBarrier cyclicBarrier;
+		private int id;
+
+		public Player(int id, CyclicBarrier cyclicBarrier) {
+			this.cyclicBarrier = cyclicBarrier;
+			this.id = id;
+		}
+
+		@Override
+		public void run() {
+			try {
+				Thread.sleep(200);
+				map.put("玩家" + id, "正在玩第一关...");
+				System.out.println("玩家" + id + "正在玩第一关...");
+				cyclicBarrier.await();
+				System.out.println("玩家" + id + "进入第二关...");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (BrokenBarrierException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	@Test
+	public void main() {
+		// CyclicBarrier cyclicBarrier = new CyclicBarrier(4);
+		CyclicBarrier cyclicBarrier = new CyclicBarrier(4, new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("所有玩家进入第二关！");
+			}
+		});
+
+		for (int i = 0; i < 4; i++) {
+			new Thread(this.new Player(i, cyclicBarrier)).start();
+		}
+		String json = new Gson().toJson(map);
+		System.out.println(json);
+	}
+}
